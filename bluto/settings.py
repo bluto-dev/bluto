abea82dc5e8b370fe78759419f2130c15f9843e4
Matt Lewis <domattthings@gmail.com>
2025-03-19 18:41:34 -0400

    Default secret key

--- bluto/settings.py
+++ bluto/settings.py
@@ -12,7 +12,7 @@ env.read_env()
 
 ENV = env.str("FLASK_ENV", default="production")
 DEBUG = ENV == "development"
-SECRET_KEY = env.str("SECRET_KEY")
+SECRET_KEY = env.str("SECRET_KEY", default="AndogfijnASDoijfAONofgijd")
 SEND_FILE_MAX_AGE_DEFAULT = env.int("SEND_FILE_MAX_AGE_DEFAULT", default=1000000)
 DEBUG_TB_ENABLED = DEBUG
 DEBUG_TB_INTERCEPT_REDIRECTS = False
